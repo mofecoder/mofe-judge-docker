54b00af54583fd9f17f06b94047a8aab1d22e125
earlgray <earlgray283@gmail.com>
2021-03-02 20:20:01 +0900

    fix: types

--- src/model.rs
+++ src/model.rs
@@ -1,44 +1,70 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
 
-#[derive(Deserialize)]
-pub struct RequestJson {
-    pub session_id: String,
-    pub cmd: String,       // コンパイルコマンド or 実行コマンド
-    pub code_path: String, // gcp 上のパス
-    pub filename: String,  // Main.ext
-    pub problem_id: usize,
-    pub time_limit: usize, // 実行制限時間
-    pub mem_limit: usize,  // メモリ制限
-
-    pub testcase: Testcase,// pub testcase: Testcase,
-    pub problem: Problem, // pub problem: Problem,
+#[derive(Deserialize, Serialize)]
+pub struct TestcaseResult {
+    pub status: Status,
+    pub cmd_result: CmdResult,
 }
 
+#[derive(Deserialize, Serialize)]
 pub struct CmdResult {
-    pub time: u128,      // ms
-    pub mem_usage: u64,  // MB
-    pub ok: bool,        // exit_code == 0
-    pub message: String, // コンパイルメッセージ
+    pub time: i32,          // ms
+    pub stdout_size: usize, // byte
+    pub mem_usage: i32,     // byte
+    pub ok: bool,           // exit_code == 0
+    pub message: String,    // コンパイルメッセージ
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, FromRow)]
 pub struct Problem {
-    pub problem_id: u64,
+    pub problem_id: i64,
     pub uuid: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, FromRow)]
 pub struct Testcase {
-    pub testcase_id: u64,
+    pub testcase_id: i64,
     pub name: String,
 }
 
+#[allow(dead_code)]
 pub struct TestcaseSets {
-    pub id: u64,
+    pub id: i64,
     pub points: u64,
 }
 
+#[allow(dead_code)]
 pub struct TestcaseTestcaseSets {
-    pub testcase_id: u64,
-    pub testcase_set_id: u64
+    pub testcase_id: i64,
+    pub testcase_set_id: i64,
+}
+
+
+#[derive(Deserialize, Serialize)]
+#[allow(clippy::upper_case_acronyms)]
+pub enum Status {
+    AC,
+    TLE,
+    MLE,
+    OLE,
+    WA,
+    RE,
+    CE,
+    IE,
+}
+
+impl Status {
+    pub fn value(&self) -> String {
+        match *self {
+            Status::AC => "AC".to_string(),
+            Status::TLE => "TLE".to_string(),
+            Status::MLE => "MLE".to_string(),
+            Status::OLE => "OLE".to_string(),
+            Status::WA => "WA".to_string(),
+            Status::RE => "RE".to_string(),
+            Status::CE => "CE".to_string(),
+            Status::IE => "IE".to_string(),
+        }
+    }
 }
