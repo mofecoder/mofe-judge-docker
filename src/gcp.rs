4860af5cfe7146d0dcbdc98dace21896515efdc4
earlgray <earlgray283@gmail.com>
2021-03-20 14:54:46 +0900

    fix: SUBMIT_SOURCE_BUCKET

--- src/gcp.rs
+++ src/gcp.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use cloud_storage::Object;
 use std::{fs::File, io::Write};
 
-const SUBMIT_SOURCE_BUCKET: &str = "cafecoder-submit-source";
+const SUBMIT_SOURCE_BUCKET: &str = "cafecoder-source";
 const TESTCASE_BUCKET: &str = "cafecoder-testcase";
 
 pub async fn download_submit_source(path: &str, name: &str) -> Result<()> {
