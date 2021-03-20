521f6b698fda4facd4f1a258a67847f3d51b344c
earlgray <earlgray283@gmail.com>
2021-03-20 16:17:27 +0900

    fix: add entry points

--- src/api/download.rs
+++ src/api/download.rs
@@ -3,7 +3,7 @@ use crate::{gcp::download_submit_source, models::DownloadRequest};
 use rocket_contrib::{json, json::Json};
 
 #[post("/download", format = "application/json", data = "<req>")]
-async fn download(req: Json<DownloadRequest>) -> ApiResponse {
+pub async fn download(req: Json<DownloadRequest>) -> ApiResponse {
     if let Err(e) = download_submit_source(&req.0.code_path, &req.0.filename).await {
         return ApiResponse::internal_server_error(e);
     };
--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,11 @@ mod sandbox;
 extern crate rocket;
 
 use anyhow::Result;
-use api::judge::judge;
+use api::{
+    judge::judge,
+    download::download,
+    compile::compile,
+};
 use config::Config;
 use once_cell::sync::Lazy;
 use std::sync::Arc;
@@ -34,7 +38,7 @@ async fn main() -> Result<()> {
 
     rocket::ignite()
         .manage(conn)
-        .mount("/", routes![judge])
+        .mount("/", routes![judge, download, compile])
         .launch()
         .await?;
 
