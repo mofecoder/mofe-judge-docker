fa97e53a7d9a904bb9f15285224eb5305990630a
earlgray <earlgray283@gmail.com>
2021-04-03 22:42:18 +0900

    fix: 0.0.0.0 で listen

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,10 @@ mod sandbox;
 extern crate rocket;
 
 use anyhow::Result;
-use api::{
-    judge::judge,
-    download::download,
-    compile::compile,
-};
+use api::{compile::compile, download::download, judge::judge};
 use config::Config;
 use once_cell::sync::Lazy;
-use std::sync::Arc;
+use std::{net::Ipv4Addr, sync::Arc};
 
 static CONFIG: Lazy<Config> = Lazy::new(|| config::load_config().unwrap());
 
@@ -29,9 +25,8 @@ const MAX_FILE_SIZE: usize = 200_000_000; // 200MB
 #[allow(dead_code)]
 const MAX_MEMORY_USAGE: i32 = 1_024_000; // 1024MB
 
-static JUDGE_DIR: Lazy<std::path::PathBuf> = Lazy::new(|| {
-    std::env::current_dir().unwrap().join("temp")
-});
+static JUDGE_DIR: Lazy<std::path::PathBuf> =
+    Lazy::new(|| std::env::current_dir().unwrap().join("temp"));
 
 #[rocket::main]
 async fn main() -> Result<()> {
@@ -40,7 +35,12 @@ async fn main() -> Result<()> {
         Arc::new(pool)
     };
 
-    rocket::ignite()
+    use rocket::config::Config;
+    let rocket_config = Config::figment()
+        .merge(("port", 8000))
+        .merge(("address",Ipv4Addr::new(0, 0, 0, 0)));
+
+    rocket::custom(rocket_config)
         .manage(conn)
         .mount("/", routes![judge, download, compile])
         .launch()
