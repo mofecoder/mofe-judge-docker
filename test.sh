ed4a10a816efef36e33bfae8638fb1b499b66159
earlgray <earlgray283@gmail.com>
2021-04-11 18:07:58 +0900

    update

--- test.sh
+++ test.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/bash
 
+CONTAINER_IP='127.0.0.1'
+
 curl \
     -X POST \
     -H 'Content-Type: application/json' \
     -d @download_request.json \
-    http://127.0.0.1:8000/download
+    http://$CONTAINER_IP:8000/download
 
 curl \
     -X POST \
     -H 'Content-Type: application/json' \
     -d @compile_request.json \
-    http://127.0.0.1:8000/compile
+    http://$CONTAINER_IP:8000/compile
 
 curl \
     -X POST \
     -H 'Content-Type: application/json' \
     -d @judge_request.json \
-    http://127.0.0.1:8000/judge
+    http://$CONTAINER_IP:8000/judge
