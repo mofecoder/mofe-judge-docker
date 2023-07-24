9161501399ea7c2e14a0b42cad7f616ee0f8c222
kichi2004 <mail@kichi2004.jp>
2023-07-24 23:38:38 +0900

    Remove userStderr.txt after compile

--- src/api/compile.rs
+++ src/api/compile.rs
@@ -20,19 +20,19 @@ pub async fn compile(req: Json<CompileRequest>, conn: &State<Arc<DbPool>>) -> Ap
     };
 
     dbg!(&cmd_res);
-
+    let stderr_path = &crate::JUDGE_DIR.join("userStderr.txt");
     if !cmd_res.ok {
-        let user_stderr_u8 =
-            std::fs::read(&crate::JUDGE_DIR.join("userStderr.txt")).unwrap_or_else(|_| Vec::new());
+        let user_stderr_u8 = std::fs::read(stderr_path).unwrap_or_else(|_| Vec::new());
         let user_stderr =
             String::from_utf8_lossy(&user_stderr_u8[..MAX_STDERR_SIZE.min(user_stderr_u8.len())]);
+        std::fs::remove_file(stderr_path).unwrap_or_else(|_| ());
         if let Err(e) = send_ce_result((*conn).clone(), req.submit_id, &user_stderr).await {
             return ApiResponse::internal_server_error(e);
         }
     }
+    std::fs::remove_file(stderr_path).unwrap_or_else(|_| ());
 
     let resp = CompileResponse(cmd_res);
-
     ApiResponse::ok(json!(resp))
 }
 
